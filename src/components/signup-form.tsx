68e9d5b01adf92983b6e19ee5a3e627731e4116f
Andualem <andualem.fereja12@gmail.com>
2025-07-05 12:52:42 +0300

    feat: add shadcn components

---
+++ src/components/signup-form.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { GoogleLogo } from "@/components/google-logo";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8, "Password must be at least 8 characters long"),
+});
+
+export function SignUpForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log();
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-xs w-full flex flex-col items-center">
+        <div className="h-9 w-9">logo</div>
+        <p className="mt-4 text-xl font-bold tracking-tight">
+          Sign up for Shadcn UI Blocks
+        </p>
+
+        <Button className="mt-8 w-full gap-3">
+          <GoogleLogo />
+          Continue with Google
+        </Button>
+
+        <div className="my-7 w-full flex items-center justify-center overflow-hidden">
+          <Separator />
+          <span className="text-sm px-2">OR</span>
+          <Separator />
+        </div>
+
+        <Form {...form}>
+          <form
+            className="w-full space-y-4"
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder="Email"
+                      className="w-full"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Password"
+                      className="w-full"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="mt-4 w-full">
+              Continue with Email
+            </Button>
+          </form>
+        </Form>
+
+        <p className="mt-5 text-sm text-center">
+          Already have an account?
+          <Link href="#" className="ml-1 underline text-muted-foreground">
+            Log in
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
