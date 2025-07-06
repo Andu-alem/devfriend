26e91ed86e18ddc910d577cfec82358c420f3a1c
Andualem <andualem.fereja12@gmail.com>
2025-07-06 19:23:53 +0300

    feat: update auth related forms to use server action

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,120 +1,79 @@
 "use client";
 
+import { useActionState } from "react";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
+import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { GoogleLogo } from "@/components/google-logo";
-import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { loginUser } from "@/lib/actions/auth-actions";
+import { Code } from "lucide-react";
 
-const formSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(8, "Password must be at least 8 characters long"),
-});
 
-export function LoginForm() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-    resolver: zodResolver(formSchema),
-  });
-
-  const onSubmit = (data: z.infer<typeof formSchema>) => {
-    console.log(data);
-  };
+export function SignUpForm() {
+  const initialState = {
+    errorMessage: ""
+  }
+  const [ state, formAction, pending ] = useActionState(loginUser,initialState)
 
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="max-w-xs w-full flex flex-col items-center">
-        <div className="h-9 w-9">Logo</div>
+        <div className="w-8 h-8 bg-gradient-to-br from-blue-600 to-purple-600 rounded-lg flex items-center justify-center">
+            <Code className="h-5 w-5 text-white" />
+        </div>
         <p className="mt-4 text-xl font-bold tracking-tight">
-          Log in to Shadcn UI Blocks
+          Wellcome back to DevFriend
         </p>
-
-        <Button className="mt-8 w-full gap-3">
+        <form
+            className="w-full space-y-4 mt-5"
+            action={formAction}
+        >
+            <div className="space-y-2">
+                <Label htmlFor="name">Email</Label>
+                <Input
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    className="w-full text-sm"
+                />
+            </div>
+            <div className="space-y-2">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                    id="password"
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    className="w-full text-sm"
+                />
+            </div>
+            <Button 
+                type="submit" 
+                className={`mt-4 w-full ${pending ? 'animate-pulse':'animate-none'}`}
+                disabled={ pending }
+            >
+              Continue with Email
+            </Button>
+        </form>
+        <div className="my-3 w-full flex items-center justify-center overflow-hidden">
+          <Separator />
+            <span className="text-sm px-2">OR</span>
+          <Separator />
+        </div>
+        <Button className="w-full gap-3">
           <GoogleLogo />
           Continue with Google
         </Button>
 
-        <div className="my-7 w-full flex items-center justify-center overflow-hidden">
-          <Separator />
-          <span className="text-sm px-2">OR</span>
-          <Separator />
-        </div>
-
-        <Form {...form}>
-          <form
-            className="w-full space-y-4"
-            onSubmit={form.handleSubmit(onSubmit)}
-          >
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="email"
-                      placeholder="Email"
-                      className="w-full"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="password"
-                      placeholder="Password"
-                      className="w-full"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit" className="mt-4 w-full">
-              Continue with Email
-            </Button>
-          </form>
-        </Form>
-
-        <div className="mt-5 space-y-5">
-          <Link
-            href="#"
-            className="text-sm block underline text-muted-foreground text-center"
-          >
-            Forgot your password?
+        <p className="mt-5 text-sm text-center">
+          Already have an account?
+          <Link href="#" className="ml-1 underline text-muted-foreground">
+            Log in
           </Link>
-          <p className="text-sm text-center">
-            Don&apos;t have an account?
-            <Link href="#" className="ml-1 underline text-muted-foreground">
-              Create account
-            </Link>
-          </p>
-        </div>
+        </p>
       </div>
     </div>
   );
--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -1,105 +1,82 @@
 "use client";
 
+import { useActionState } from "react";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
+import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { GoogleLogo } from "@/components/google-logo";
-import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { signupUser } from "@/lib/actions/auth-actions";
+import { Code } from "lucide-react";
 
-const formSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(8, "Password must be at least 8 characters long"),
-});
 
 export function SignUpForm() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-    resolver: zodResolver(formSchema),
-  });
-
-  const onSubmit = (data: z.infer<typeof formSchema>) => {
-    console.log();
-  };
+  const initialState = {
+    errorMessage: ""
+  }
+  const [ state, formAction, pending ] = useActionState(signupUser,initialState)
 
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="max-w-xs w-full flex flex-col items-center">
-        <div className="h-9 w-9">logo</div>
+        <div className="w-8 h-8 bg-gradient-to-br from-blue-600 to-purple-600 rounded-lg flex items-center justify-center">
+            <Code className="h-5 w-5 text-white" />
+        </div>
         <p className="mt-4 text-xl font-bold tracking-tight">
-          Sign up for Shadcn UI Blocks
+          Sign up for DevFriend
         </p>
-
-        <Button className="mt-8 w-full gap-3">
+        <form
+            className="w-full space-y-4 mt-5"
+            action={formAction}
+        >
+            <div className="space-y-2">
+                <Label htmlFor="name">Full Name</Label>
+                <Input
+                    id="name"
+                    name="name"
+                    type="text"
+                    placeholder="Full Name"
+                    className="w-full text-sm"
+                />
+            </div>
+            <div className="space-y-2">
+                <Label htmlFor="name">Email</Label>
+                <Input
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    className="w-full text-sm"
+                />
+            </div>
+            <div className="space-y-2">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                    id="password"
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    className="w-full text-sm"
+                />
+            </div>
+            <Button 
+                type="submit" 
+                className={`mt-4 w-full ${pending ? 'animate-pulse':'animate-none'}`}
+                disabled={ pending }
+            >
+              Continue with Email
+            </Button>
+        </form>
+        <div className="my-3 w-full flex items-center justify-center overflow-hidden">
+          <Separator />
+            <span className="text-sm px-2">OR</span>
+          <Separator />
+        </div>
+        <Button className="w-full gap-3">
           <GoogleLogo />
           Continue with Google
         </Button>
-
-        <div className="my-7 w-full flex items-center justify-center overflow-hidden">
-          <Separator />
-          <span className="text-sm px-2">OR</span>
-          <Separator />
-        </div>
-
-        <Form {...form}>
-          <form
-            className="w-full space-y-4"
-            onSubmit={form.handleSubmit(onSubmit)}
-          >
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="email"
-                      placeholder="Email"
-                      className="w-full"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="password"
-                      placeholder="Password"
-                      className="w-full"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit" className="mt-4 w-full">
-              Continue with Email
-            </Button>
-          </form>
-        </Form>
 
         <p className="mt-5 text-sm text-center">
           Already have an account?
