f7a03924d679287f3fe5799deff855ec14cf38d9
Andualem <andualem.fereja12@gmail.com>
2025-07-07 09:50:34 +0300

    feat: implement schema validation using zod

--- src/lib/actions/auth-actions.ts
+++ src/lib/actions/auth-actions.ts
@@ -1,22 +1,65 @@
 'use server'
 import { db } from '@/db/drizzle'
 import { auth } from '../auth'
+import * as z from 'zod'
+
+
+const signupSchema = z.object({
+    name: z.string().max(15),
+    email: z.string().email(),
+    password: z.string().min(8)
+})
+
+const loginSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8)
+})
 
 export async function signupUser(prevState:any, formData: FormData) {
-    const fullName = formData.get('name')
-    const email = formData.get('email')
-    const password = formData.get('password')
-    console.log(`Name: ${fullName}, email: ${email}, password: ${password}`)
-    return {
-        errorMessage: 'error'
+    const rawFormData = Object.fromEntries(formData)
+    const { name, email, password } = rawFormData
+
+    const validateData = signupSchema.safeParse({
+        name,
+        email,
+        password
+    })
+
+    if (!validateData.success) {
+        return {
+            errorMessage: "Invalid data"
+        }
+    }
+
+    try {
+    } catch(error) {
+        console.log("error occlured")
+        return {
+            errorMessage: ""
+        }
     }
 }
 
 export async function loginUser(prevState:any, formData: FormData) {
-    const email = formData.get('email')
-    const password = formData.get('password')
-    console.log(`Email: ${email}, password: ${password}`)
-    return {
-        errorMessage: 'error'
+    const rawFormData = Object.fromEntries(formData)
+    const { email, password } = rawFormData
+
+    const validateData = loginSchema.safeParse({
+        email,
+        password
+    })
+
+    if (!validateData.success) {
+        return {
+            errorMessage: "Invalid data"
+        }
+    }
+
+    try {
+    } catch(error) {
+        console.log("error occlured")
+        return {
+            errorMessage: ""
+        }
     }
 }
