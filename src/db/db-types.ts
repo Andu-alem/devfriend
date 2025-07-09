90c120c6105226a4a33bf6aa44ba42f888adf443
Andualem <andualem.fereja12@gmail.com>
2025-07-09 14:56:33 +0300

    feat: export zod schemas

--- src/db/db-types.ts
+++ src/db/db-types.ts
@@ -1,8 +1,14 @@
 import { InferSelectModel, InferInsertModel, InferEnum } from "drizzle-orm";
+import { 
+    createSelectSchema,
+    createInsertSchema,
+    createUpdateSchema
+} from "drizzle-zod";
 
 import { jobStatusEnum, jobs } from "./schema/jobs-schema";
 import { eventTypeEnum, eventPriorityEnum, events } from "./schema/events-schema";
 import { projectStatusEnum, projects } from "./schema/projects-schema";
+
 
 
 // generating types from db schema
@@ -20,3 +26,16 @@ export type NewEvent = InferInsertModel<typeof events>;
 export type ProjectStatus = InferEnum<typeof projectStatusEnum>;
 export type Project = InferSelectModel<typeof projects>;
 export type NewProject = InferInsertModel<typeof projects>;
+
+
+// zod schemas
+export const projectSchema = createInsertSchema(projects);
+export const projectSelectSchema = createSelectSchema(projects);
+export const projectUpdateSchema = createUpdateSchema(projects);
+export const eventInsertSchema = createInsertSchema(events);
+export const eventSelectSchema = createSelectSchema(events);
+export const eventUpdateSchema = createUpdateSchema(events);
+export const jobSelectSchema = createSelectSchema(jobs);
+export const jobInsertSchema = createInsertSchema(jobs);
+export const jobUpdateSchema = createUpdateSchema(jobs);
+
