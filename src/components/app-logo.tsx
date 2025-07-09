97b95a26d52b276dc716a793823edd419f85598e
Andualem <andualem.fereja12@gmail.com>
2025-07-09 21:58:05 +0300

    feat: create an app logo

---
+++ src/components/app-logo.tsx
@@ -0,0 +1,7 @@
+export function AppLogo() {
+    return (
+        <div className="w-8 h-8 bg-gradient-to-br from-blue-600 to-purple-600 rounded-lg flex items-center justify-center">
+            <Code className="h-5 w-5 text-white" />
+        </div>
+    )
+}
