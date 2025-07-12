00035298e742b8b18076258c9914627fdd562245
Andualem <andualem.fereja12@gmail.com>
2025-07-12 14:37:06 +0300

    feat: create an api route to fetch projects related to specified, authenticated users

---
+++ src/app/api/projects/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse, NextRequest } from "next/server";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
+import { db } from "@/db/drizzle";
+import { projects } from "@/db/schema/projects-schema";
+import { desc, eq } from "drizzle-orm";
+
+export async function GET(request: NextRequest) {
+    const authData = await auth.api.getSession({
+        headers: await headers(),
+    })
+    
+    if(!authData?.user) {
+        return NextResponse.json(
+            { error: 'Unauthorized access' },
+            { status: 401 }
+        )
+    }
+    try {
+        const result = await db
+            .select()
+            .from(projects)
+            .where(eq(projects.userId, authData.user.id))
+            .orderBy(desc(projects.createdAt))
+
+        return NextResponse.json(result)
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Couldn't access the database" },
+            { status: 500 }
+        )
+    }
+}
