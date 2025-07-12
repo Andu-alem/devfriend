5a533d5ca6fc617b82771935ff3b657a6eb7a8dd
Andualem <andualem.fereja12@gmail.com>
2025-07-12 14:35:26 +0300

    feat: create an api route to fetch events for a specified, authenticated users

---
+++ src/app/api/events/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
+import { db } from "@/db/drizzle";
+import { events } from "@/db/schema/events-schema";
+import { desc, eq } from "drizzle-orm";
+
+export async function GET() {
+    const authData = await auth.api.getSession({
+        headers: await headers()
+    })
+    if(!authData?.user) {
+        return NextResponse.json(
+            { error: 'Unauthorized access' },
+            { status: 401 }
+        )
+    }
+    try {
+        const result = await db
+            .select()
+            .from(events)
+            .where(eq(events.userId, authData.user.id))
+            .orderBy(desc(events.date))
+
+        return NextResponse.json(result)
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Couldn't access the database" },
+            { status: 500 }
+        )
+    }
+}
