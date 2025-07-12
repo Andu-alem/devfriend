5d4aca540e073dbe23faa12f390035d43d4aad97
Andualem <andualem.fereja12@gmail.com>
2025-07-12 14:36:24 +0300

    feat: create an api route to fetch jos related to specified, authenticated users

---
+++ src/app/api/jobs/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
+import { db } from "@/db/drizzle";
+import { jobs } from "@/db/schema/jobs-schema";
+import { desc, eq } from "drizzle-orm";
+
+export async function GET() {
+    const authData = await auth.api.getSession({
+        headers: await headers()
+    })
+    if(!authData?.user) {
+        return NextResponse.json(
+            { error: 'Unauthorized access' },
+            { status: 401 }
+        )
+    }
+    try {
+        const result = await db
+            .select()
+            .from(jobs)
+            .where(eq(jobs.userId, authData.user.id))
+            .orderBy(desc(jobs.createdAt))
+
+        return NextResponse.json(result)
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Couldn't access the database" },
+            { status: 500 }
+        )
+    }
+}
