126215f58a9531eedc9588ee26c2ee347aeaa8bb
Andualem <andualem.fereja12@gmail.com>
2025-07-13 23:23:12 +0300

    chore: add neon db driver

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,6 +1,10 @@
 import dotenv from 'dotenv';
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+//import { drizzle } from 'drizzle-orm/node-postgres';
 
 dotenv.config({ path: './.env.local' })
 
-export const db = drizzle(process.env.DATABASE_URL || "");
+//export const db = drizzle(process.env.DATABASE_URL || "");
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle({ client: sql });
